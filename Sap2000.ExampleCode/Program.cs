b75a9a5416fc333edcbe063599cb2e433af0df91
Cynthia59 <cindydai59@163.com>
2019-01-05 11:24:48 +0800

    添加项目文件。

---
+++ Sap2000.ExampleCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sap2000.ExampleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
